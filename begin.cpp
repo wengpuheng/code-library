c9879d4efdbe1c93ee022c6d1fe4b2ad50c63f90
puhengweng <wengpuhengwph@gmail.com>
2022-08-09 08:49:28 +0800

    update

--- begin.cpp
+++ begin.cpp
@@ -2,8 +2,15 @@
 
 using namespace std;
 
+#ifdef LOCAL
+#include "debug.h"
+#else
+#define debug(...) 42
+#endif
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr); 
+	int a, b; 
 	return 0; 
 }
